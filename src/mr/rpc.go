785725105f82a32c157d7868e7a28b31c12c95da
mwt <mwt@>
2025-09-17 02:51:03 +0000

    新增rpc

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -13,6 +13,30 @@ import "strconv"
 // example to show how to declare the arguments
 // and reply for an RPC.
 //
+// RPC请求任务的参数和回复结构体
+type RequestTaskArgs struct {
+	WorkerId int
+}
+
+type RequestTaskReply struct {
+	TaskType   string // "map", "reduce", "wait", "done"
+	TaskId     int
+	FileName   string
+	NReduce    int
+	NMap       int
+}
+
+// RPC报告任务完成的参数和回复结构体
+type ReportTaskDoneArgs struct {
+	WorkerId   int
+	TaskType   string // "map" or "reduce"
+	TaskId     int
+}
+
+type ReportTaskDoneReply struct {
+	OK bool
+}
+
 
 type ExampleArgs struct {
 	X int
